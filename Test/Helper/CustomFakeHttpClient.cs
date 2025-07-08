5435f08a3f51b04f01be065fb0d234d071694939
bastolamanoj <manojbastola676@gmail.com>
2025-07-08 23:40:59 +0545

    Make a setup to fake http sevices

---
+++ Test/Helper/CustomFakeHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using FakeItEasy;
+using System.Net;
+using System.Text.Json;
+
+namespace Test.Helper
+{
+    public class CustomFakeHttpClient
+    {
+        public static HttpClient FakeHttpClient<T>(T content)
+        {
+            var response = new HttpResponseMessage
+            {
+                //StatusCode = System.Net.HttpStatusCode.OK,
+                //Content = new StringContent(System.Text.Json.JsonSerializer.Serialize(content), Encoding.UTF8, "application/json")
+                Content = new StringContent(JsonSerializer.Serialize(content))
+            };
+
+            var handler = A.Fake<FakeableHttpMessageHandler>();
+            A.CallTo(() => handler.FakeSendAsync(A<HttpRequestMessage>.Ignored, A<CancellationToken>.Ignored))
+            .Returns(response);
+            return new HttpClient(handler);
+
+        }
+
+    }
+}
---
+++ Test/Helper/FakeableHttpMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Helper
+{
+    public abstract class FakeableHttpMessageHandler : HttpMessageHandler
+    {
+        public abstract Task<HttpResponseMessage> FakeSendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+        protected sealed override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            => this.FakeSendAsync(request, cancellationToken);
+    }
+}
