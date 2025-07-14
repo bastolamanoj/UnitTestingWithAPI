cf152f05de63279068c4bcc005096c1f72066084
bastolamanoj <manojbastola676@gmail.com>
2025-07-14 23:21:07 +0545

    update configuration for production to show swagger

--- UnitTestingWithAPI/Program.cs
+++ UnitTestingWithAPI/Program.cs
@@ -20,12 +20,12 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.MapOpenApi();
     app.UseSwaggerUI(opt =>
        opt.SwaggerEndpoint("/openapi/v1.json", "API v1"));
-}
+//}
 
 app.UseHttpsRedirection();
 
